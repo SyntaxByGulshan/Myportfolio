82b22c9a205316a79a374441964f35bcecd32eba
Gulshan kumar <gul19june@gmail.com>
2025-10-09 21:31:02 +0530

    update 30(Hamburger fix)

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import SecondPhoto from "../costomHook/SecondPhoto";
 
 export default function About() {
